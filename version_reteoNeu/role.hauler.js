b01fd3aff6ed905a560397d5eb79ecf7629f9b4e
Reteoteigam <reteoteigam+coding@gmail.com>
2026-01-06 22:17:49 +0100

    feat: implement container mining logic for miners and haulers

--- version_reteoNeu/role.hauler.js
+++ version_reteoNeu/role.hauler.js
@@ -3,46 +3,49 @@ const LOGGER = require('util.log');
 var roleHauler = {
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
-            var droppedEnergy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
-            if(droppedEnergy) {
-                if(creep.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(droppedEnergy, {visualizePathStyle: {stroke: '#ffaa00'}});
-                    creep.say('🛒');
+            // 1. Energiequelle suchen: Erst Container, dann Boden
+            var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: s => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
+            });
+
+            if(container) {
+                if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    creep.say('📦 Cont');
+                }
+            } else {
+                // Fallback: Energie am Boden
+                var dropped = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+                if(dropped) {
+                    if(creep.pickup(dropped) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(dropped);
+                        creep.say('🛒 Boden');
+                    }
                 }
             }
         } else {
-            // ZIEL-PRIORISIERUNG
-            // 1. Strukturen (Spawn/Extensions)
-            var structureTarget = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            // LIEFERLOGIK (bleibt gleich wie vorher: Spawn -> Worker)
+            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (s) => (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION)
                     && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
             });
 
-            if(structureTarget) {
-                if(creep.transfer(structureTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structureTarget, {visualizePathStyle: {stroke: '#ffffff'}});
+            if(target) {
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                     creep.say('🏦');
                 }
-            }
-            // 2. Wenn Strukturen voll: Belieferung von arbeitenden Creeps (Upgrader/Builder)
-            else {
-                var workerTarget = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+            } else {
+                // Belieferung von Upgradern/Buildern
+                var worker = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
                     filter: (c) => (c.memory.role == 'upgrader' || c.memory.role == 'builder')
-                        && c.store.getFreeCapacity(RESOURCE_ENERGY) > (c.store.getCapacity() * 0.5)
+                        && c.store.getFreeCapacity(RESOURCE_ENERGY) > 0
                 });
-
-                if(workerTarget) {
-                    if(creep.transfer(workerTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(workerTarget, {visualizePathStyle: {stroke: '#00ff00'}});
-                        creep.say('🚚 ' + workerTarget.name);
-                    } else {
-                        LOGGER.debug(creep.name + " liefert Energie an " + workerTarget.name);
+                if(worker) {
+                    if(creep.transfer(worker, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(worker);
+                        creep.say('🚚');
                     }
-                } else {
-                    creep.say('😴');
-                    // Parken am Spawn, um nicht im Weg zu stehen
-                    var spawn = Game.spawns['HQ'] || Object.values(Game.spawns)[0];
-                    creep.moveTo(spawn, {visualizePathStyle: {stroke: '#555555'}});
                 }
             }
         }
--- version_reteoNeu/role.miner.js
+++ version_reteoNeu/role.miner.js
@@ -1,10 +1,33 @@
+const LOGGER = require('util.log');
+
 var roleMiner = {
     run: function(creep) {
-        // Der Miner bekommt im Memory eine feste Source-ID zugewiesen
         var source = Game.getObjectById(creep.memory.sourceId);
-        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+
+        // 1. Suche einen Container direkt neben der Quelle
+        var container = source.pos.findInRange(FIND_STRUCTURES, 1, {
+            filter: s => s.structureType == STRUCTURE_CONTAINER
+        })[0];
+
+        // 2. Positionierung: Wenn ein Container da ist, stell dich drauf
+        if(container) {
+            if(!creep.pos.isEqualTo(container.pos)) {
+                creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.say('🚶 Basis');
+            }
+        } else {
+            // Falls kein Container da ist, geh einfach zur Quelle
+            if(!creep.pos.isNearTo(source)) {
+                creep.moveTo(source);
+            }
+        }
+
+        // 3. Ernten (Energie fällt automatisch in den Container, wenn man draufsteht)
+        if(creep.harvest(source) == OK) {
+            // Wir loggen nur alle 50 Ticks, um das Log nicht zu fluten
+            if(Game.time % 50 === 0) LOGGER.debug(creep.name + " erntet fleißig.");
         }
     }
 };
+
 module.exports = roleMiner;
